52d481b2647b97b2131abe482f5a8d758ec8791a
JoakimHaglund <joakimhaglund@outlook.com>
2025-04-07 20:28:33 +0200

    Added name info class

---
+++ FormatNameLists/NameInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FormatNameLists
+{
+    class NameInfo
+    {
+        private string name;
+        public required string Name
+        {
+            get { return name; }
+            set
+            {
+                // Gör om första bokstaven till stor, resten till små bokstäver
+                name = char.ToUpper(value[0]) + value.Substring(1).ToLower();
+            }
+        }
+        public int Antal { get; set; }
+        public string? DescriptionOfName { get; set; }
+        public bool IsBoy { get; set; }
+    }
+}
