2c4ee133c7e2f60d034a62cf3bb7c7f610ca036a
JoakimHaglund <joakimhaglund@outlook.com>
2025-04-17 06:50:15 +0200

    Setup DbContext

---
+++ NameSpace/DBContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NameSpace.Models;
+
+
+namespace NameSpace
+{
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } // Användarnas tabell
+        public DbSet<NameInfo> NameInfos { get; set; }
+
+        public DbSet<UserReaction> UserReactions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<NameInfo>()
+                .HasIndex(s => new {s.Name, s.Gender})
+                .IsUnique();
+
+            //User reactions config
+            modelBuilder.Entity<UserReaction>()
+           .HasKey(tc => new { tc.UserId, tc.NameInfoId }); // Primärnyckel är sammansatt av UserId och PostId
+
+            modelBuilder.Entity<UserReaction>()
+                .HasOne(tc => tc.User)
+                .WithMany(u => u.UserReactions)
+                .HasForeignKey(tc => tc.UserId);
+
+            modelBuilder.Entity<UserReaction>()
+                .HasOne(tc => tc.NameInfo)
+                .WithMany(p => p.UserReactions)
+                .HasForeignKey(tc => tc.NameInfoId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    } 
+}
