75090733e7803c28865593a3bf6ccd2c62ea4ea1
JoakimHaglund <joakimhaglund@outlook.com>
2025-04-23 10:59:04 +0200

    Added api calls

--- Frontend/scripts/api.js
+++ Frontend/scripts/api.js
@@ -8,16 +8,54 @@ const apiClient = axios.create({
     withCredentials: true, 
 });
 
-export const getData = async (letter) => {
+export const getNamesByLetter = async (letter, pageNumber = 1, pageSize = 50) => {
     try{
+        console.log(letter, pageNumber);
         const response = await apiClient.get('/Names/by-letter', {
             params: {
-                letter
+                letter,
+                pageNumber,
+                pageSize,
+                minCount: 100,
             }
         });
         return response.data;
     } catch (error) {
-        console.error('Whalla din wahab! ', error);
+        console.error('error getting names ', error);
+        throw error;
+    }
+};
+export const loginUser = async (username, password) => {
+    try{
+        const response = await apiClient.post('/Account/login', {
+          username: username,
+          password: password,
+        });
+        return response; // Hämta namnplattor
+    }catch (error) {
+        console.error('login error ', error);
+        return error.response.data;
+    }
+};
+export const addReactions = async (reactions) => {
+    try{
+        const response = await apiClient.post('/Reactions/reactions', reactions);
+        return response;
+    }catch (error) {
+        console.error('Reaction http error ', error);
+        return error.response.data;
+    }
+};
+export const getReactions = async (reaction) => {
+    try{
+        const response = await apiClient.get('/Reactions/reactions', {
+            params: {
+                reaction: reaction,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('error getting names ', error);
         throw error;
     }
 };
