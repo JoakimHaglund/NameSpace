205ee8728f49a12d6243d5e91c9dc6d3be5d4873
JoakimHaglund <joakimhaglund@outlook.com>
2025-05-06 12:16:56 +0200

    Added partner to user

--- NameSpace/Models/User.cs
+++ NameSpace/Models/User.cs
@@ -5,6 +5,8 @@ namespace NameSpace.Models
     public class User : IdentityUser
     {
         public ICollection<UserReaction>? UserReactions { get; set; }
+        public string? PartnerUserId { get; set; }
+        public User? PartnerUser { get; set; }
 
     }
 }
