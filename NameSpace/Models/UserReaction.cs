92ee9df1badad86782e69adc6b77984991b3e538
JoakimHaglund <joakimhaglund@outlook.com>
2025-05-06 12:18:08 +0200

    REmoved bool "IsAFavorite" now part of Reaction enum

--- NameSpace/Models/UserReaction.cs
+++ NameSpace/Models/UserReaction.cs
@@ -5,14 +5,14 @@
         public required string UserId { get; set; }
         public required Guid NameInfoId { get; set; }
         public required ReactionType Reaction { get; set; }
-        public bool IsAFavorite { get; set; } = false;
 
         public User? User { get; set; }
         public NameInfo? NameInfo { get; set; }
     }
     public enum ReactionType
     {
-        Like = 1,
         Dislike = 0,
+        Like = 1,
+        Favorite = 2,
     }
 }
