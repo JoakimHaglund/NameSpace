8e8cd047ad69241efe151ba7359e3ee618f3a155
JoakimHaglund <joakimhaglund@outlook.com>
2025-05-06 12:19:11 +0200

    Added model for partner requests

---
+++ NameSpace/Models/ConfirmPartner.cs
@@ -0,0 +1,10 @@
+﻿namespace NameSpace.Models
+{
+    public class ConfirmPartner
+    {
+        public required Guid Id { get; set; }
+        public required Guid Token { get; set; }
+        public required string RequestingUserId { get; set; }
+        public required string RecivingUserId { get; set; }
+    }
+}
