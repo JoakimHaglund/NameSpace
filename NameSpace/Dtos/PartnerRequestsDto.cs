1763092096db616d17c9a4f46582bb93fd6b9829
JoakimHaglund <joakimhaglund@outlook.com>
2025-06-08 16:12:30 +0200

    Added Dto for partner requests

---
+++ NameSpace/Dtos/PartnerRequestsDto.cs
@@ -0,0 +1,17 @@
+﻿namespace NameSpace.Dtos
+{
+    public class PartnerRequestsDto
+    {
+        public string? CurrentPartner { get; set; }
+        public List<PartnerRequestSimple>? MyPartnerRequests { get; set; }
+        public List<PartnerRequestSimple>? PartnerRequests { get; set; }
+
+    }
+
+    public class PartnerRequestSimple
+    {
+        public Guid Id { get; set; }
+        public required string UserId { get; set; }
+    }
+
+}
