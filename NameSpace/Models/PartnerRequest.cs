9ed792a4f0295822c160742f391447443afa89fd
JoakimHaglund <joakimhaglund@outlook.com>
2025-06-08 16:13:01 +0200

    Renamed model

--- NameSpace/Models/PartnerRequest.cs
+++ NameSpace/Models/PartnerRequest.cs
@@ -1,9 +1,8 @@
 ﻿namespace NameSpace.Models
 {
-    public class ConfirmPartner
+    public class PartnerRequest
     {
         public required Guid Id { get; set; }
-        public required Guid Token { get; set; }
         public required string RequestingUserId { get; set; }
         public required string RecivingUserId { get; set; }
     }
