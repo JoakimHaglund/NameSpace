e932b5fe82528d5aad4a018c5e5a6edf3ab98b60
JoakimHaglund <joakimhaglund@outlook.com>
2025-11-19 14:29:26 +0100

    moved routing logic to the router and cleanup

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,36 +1,9 @@
 
 <template>
-  <RouterView v-if="!state.isCheckingLogin" />
+    <router-view />
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView, useRouter } from 'vue-router'
-import { onMounted } from 'vue'
-import * as api from './scripts/api.ts';
-import {state} from './scripts/state.ts'
-const router = useRouter()
-
-onMounted(async () => {
-  try {
-    const response = await api.checkLoginStatus()
-    if (state.isLoggedIn) {
-      router.push('/menu')
-    } else {
-      router.push('/login')
-    }
-  } catch (err) {
-    console.error("Kunde inte kolla login:", err)
-    state.isLoggedIn = false
-    router.push('/login')
-  } finally {
-    state.isCheckingLogin = false
-  }
-})
-
+import { RouterView } from 'vue-router';
 </script>
-
-
-
-<style scoped>
-
-</style>
+<style scoped></style>
