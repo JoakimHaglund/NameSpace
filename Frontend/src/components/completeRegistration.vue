b5818e078287747e793f1d21a9f091345504e4a4
JoakimHaglund <joakimhaglund@outlook.com>
2025-12-01 14:51:52 +0100

    Added time and basic style

--- Frontend/src/components/completeRegistration.vue
+++ Frontend/src/components/completeRegistration.vue
@@ -1,31 +1,53 @@
 <template>
     <h1 class="gradiant-text">NameSpace</h1>
-    <p class="gradiant-text">reg {{status}}</p>
+    <p class="gradiant-text register-message">{{ status }}</p>
+    <p v-if="status" class="gradiant-text register-message">Du skickas vidare automatiskt om {{ seconds }}</p>
 </template>
 
 <script setup lang="ts">
 import * as api from '@scripts/api.ts';
 import { onMounted, ref } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
-console.log('reg laddas');
-const status = ref('nostatus')
+const route = useRoute();
+const router = useRouter();
+const status = ref('');
+const token = route.query.token;
+const email = route.query.email;
+const seconds = ref(5);
 
-    onMounted(async () => { //behöver hämta min data någon ananstans 
-        // Hämta query-param direkt från URL:n
-        const route = useRoute()
-        const token = route.query.token  // Hämtar 'token' från URL:en
-        const email = route.query.email  // Hämtar 'email' från URL:en
-
-        if (token && email){
+onMounted(async () => {
+    try {
+        if (token && email) {
             console.log('Token finns:', token);
             status.value = await api.completeRegistration(token.toString(), email.toString());
             console.log('status:', status);
         }
-        else{
+        else {
             console.log('Token finns inte');
+            status.value = 'Token finns inte';
         }
+    }
+    catch (error) {
+        console.log('API-anrop misslyckades:', error);
+        status.value = 'Verifieringen misslyckades, försök igen senare';
+    }
+    finally {
+        const timer = setInterval(() => {
+            seconds.value--;
+            if (seconds.value === 0) {
+                clearInterval(timer);
+                router.push('/login');
+            }
+        }, 1000)
+    }
 
-    });
+});
 </script>
-<style scoped></style>
+<style scoped>
+.register-message {
+    text-align: center;
+    font-weight: bolder;
+    font-size: 3rem;
+}
+</style>
