ed9e003d871c9f0db52f030f053eef693e4245fe
JoakimHaglund <joakimhaglund@outlook.com>
2025-12-01 14:52:58 +0100

    Removed comments

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,52 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { checkLoginStatus } from '../scripts/api.ts';
-import {state} from '../scripts/state.ts';
+import { state } from '../scripts/state.ts';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   /* {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-  /*  {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },*/
-     {
+    {
       path: '/swipecard',
       name: 'swipecard',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../pages/tinder/index.vue'),
     },
-     {
+    {
       path: '/login',
       name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../pages/login/index.vue'),
     },
     {
       path: '/confirm-email',
       name: 'confirm-email',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../components/completeRegistration.vue'),
     },
     {
-     path: '/menu',
+      path: '/menu',
       name: 'menu',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../pages/main-menu/index.vue'),
       children: [
         {
@@ -64,19 +39,13 @@ const router = createRouter({
       ]
     },
     {
-     path: '/card',
+      path: '/card',
       name: 'card',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../pages/tinder/index.vue'),
     },
     {
-     path: '/list/:list',
+      path: '/list/:list',
       name: 'list',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../pages/lists/list.vue'),
       props: true
 
